c89559849eb88a10f119a6ad53d35ead08e8cb6d
Iain R. Learmonth <irl@fsfe.org>
2014-08-10 23:38:05 +0100

    Produce JSON file with sample data from RTL-SDR

--- app.py
+++ app.py
@@ -7,13 +7,47 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 #######################################################################
 
-from flask import Flask
+import numpy as np
+from rtlsdr import RtlSdr
+import time, threading
+from flask import Flask, jsonify
+
+NFFT = 1024*4
+NUM_SAMPLES_PER_SCAN = NFFT*16
+
+class Radio(object):
+
+    def __init__(self, sdr):
+        self.sdr = sdr
+
+    def updateSamples(self):
+        self.samples = self.sdr.read_samples(NUM_SAMPLES_PER_SCAN)
+        threading.Timer(0.2, self.updateSamples).start()
+
+    def getSamples(self):
+        return [np.absolute(x).tolist() for x in self.samples.tolist()]
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "WebSDR-Plus!"
+    return "WebSDR-Plus!"
+
+@app.route("/2m.json")
+def hello():
+    return jsonify({'samples': radio.getSamples(), 'minf': 144.0e6, 'maxf': 146.0e6})
 
 if __name__ == "__main__":
-        app.run()
+    sdr = RtlSdr()
+    radio = Radio(sdr)
+
+    # some defaults
+    sdr.rs = 2.0e6
+    sdr.fc = 145.0e6
+    sdr.gain = 10
+
+    # start polling for samples
+    radio.updateSamples()
+
+    app.run()
 
